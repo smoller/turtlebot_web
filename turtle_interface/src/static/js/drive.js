f13b4e7788a1103ef1040dcc84a467e1197c3c33
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2014-09-21 11:18:25 +1000

    tried key events in move.html. canvas showing.

--- turtle_interface/src/static/js/drive.js
+++ turtle_interface/src/static/js/drive.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/drive'); 
-    Gamecontroller.init({
-        left: {type: 'joystick'},
+    GameController.init({
+        left: {
+            type: 'joystick',
+            touchMove: function(details) {
+                socket.emit('move', {x:details.normalizedX, y:details.normalizedY});
+            }
+        },
         right: {}
-        touchMove: function(details) {
-            socket.emit('move', {x:details.normalizedX, y:details.normalizedY});
-        }
     });
 });
