cea9aa1e4dc030b808120815ebb2c913b6f5b974
Simon Moller <Simon@Simons-MacBook-Air.local>
2014-09-25 11:50:21 +1000

    Added annyang voice commands

--- turtle_interface/src/static/js/drive.js
+++ turtle_interface/src/static/js/drive.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
     });
 
     $("#up").click(function() {
-        console.log("Hits");
         socket.emit('move', {x:0, y:1});
     });
 
@@ -26,4 +25,32 @@ $(document).ready(function() {
     $("#right").click(function() {
         socket.emit('move', {x:-1, y:0});
     });
+
+    var voice_commands = {
+        'move :direction' : move,
+        'turn :direction' : turn,
+        'dance' : dance
+    }
+
+    var move = function(direction) {
+        var backwards = ['backward', 'back'];
+        var forwards = ['forward', 'forwards', 'ahead', 'forth', 'on'];
+        if ($.inArray(direction, forwards) > -1) {
+            socket.emit('move', {x:0, y:1});
+        } else if ($.inArray(direction, backwards) > -1) {
+            socket.emit('move', {x:0, y:-1});
+        } else {
+            console.log("I don't know that direction");
+        }
+    }
+
+    var turn = function(direction) {
+        if (direction === 'left') {
+            socket.emit('move', {x:1, y:0});
+        } else if (direction === 'right') {
+            socket.emit('move', {x:-1, y:0});
+        } else {
+            console.log("I don't know that direction");
+        }
+    }
 });
