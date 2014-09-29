44194a2add232df23e1080426a49080fd64152d7
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2014-09-29 17:10:08 +1000

    basic object detection, tied into teleop

--- turtle_handlers/src/turtle_handlers/turtle_teleop.py
+++ turtle_handlers/src/turtle_handlers/turtle_teleop.py
@@ -1,10 +1,11 @@
 import rospy
-
 from geometry_msgs.msg import Twist
+
+from obstacle_detector import ObstacleDetector
 
 class TurtleTeleOp(object):
 
-    def __init__(self, topic='cmd_vel_mux/input/navi'):
+    def __init__(self, topic='cmd_vel_mux/input/navi', obstacle_detect=True):
         rospy.init_node('turtlebot_move', anonymous=True)
         self.pub = rospy.Publisher(topic, Twist, queue_size=1)
         self.twist = Twist()
@@ -12,6 +13,8 @@ class TurtleTeleOp(object):
         self._max_speed = 0.4 # m/s
         self._max_omega = 0.3 # rad/s
         self._range = (-1, 1)
+        self.obstacle_detect = obstacle_detect
+        if obstacle_detect: obs_detector = ObstacleDetector()
 
     def set_move(self, speed, omega):
         self.twist.linear.x, self.twist.angular.z = speed, omega
@@ -20,6 +23,14 @@ class TurtleTeleOp(object):
         # receive range in [-1, 1]
         speed = self._clamp(y, *self._range) * self._max_speed
         omega = self._clamp(x, *self._range) * self._max_omega
+
+        # check for obstacles if obstacle detection enabled
+        if self.obstacle_detect and self.obs_detector.is_obstacle():
+            rospy.loginfo('obstacle detected!')
+            if speed > 0:
+                # only stop if move is invalid, ie. moving towards obstacle
+                speed = omega = 0
+                rospy.loginfo('invalid move!')
 
         self.set_move(speed, omega)
         self.publish()
