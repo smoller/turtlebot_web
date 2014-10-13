f45aa3c7458854dc59faad1e7f2faf6fbbb48414
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2014-10-13 13:17:23 +1100

    trying to test show map as image

---
+++ turtle_handlers/src/turtle_handlers/map_pub.py
@@ -0,0 +1,16 @@
+import rospy
+from nav_msgs.msg import OccupancyGrid
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('map', OccupancyGrid, queue_size=1)
+    rospy.init_node('test_map_pub', anonymous=True)
+    r = rospy.Rate(1)
+    map_msg = OccupancyGrid()
+    map_msg.info.height = map_msg.info.width = 2
+    flag = True
+
+    while not rospy.is_shutdown():
+        map_msg.data = [0,100,0,100] if flag else [100,0,100,0]
+        pub.publish(map_msg)
+        flag = not flag
+        r.sleep()
