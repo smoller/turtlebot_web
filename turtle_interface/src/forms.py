44a55a13f7af0a7cb61e5841eacd81d0e841caba
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2014-10-25 19:27:25 +1100

    dynamic add waypoints. no remove...

--- turtle_interface/src/forms.py
+++ turtle_interface/src/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, TextAreaField, FormField, FieldList, SubmitField
+from wtforms import TextField, TextAreaField, FormField, FieldList
 from wtforms.validators import Required
 
 class WaypointForm(Form):
@@ -7,7 +7,6 @@ class WaypointForm(Form):
     script = TextAreaField('Script')
     def __init__(self, *args, **kwargs):
         super(WaypointForm, self).__init__(csrf_enabled=False, *args, **kwargs)
-
 
 class TourForm(Form):
     name = TextField('Name', validators=[Required()])
