9a4d2bd9d0fa0d1ea8d552ed5ccc73258d4ff9dd
andrewm3 <2037905+andrewm3@users.noreply.github.com>
2014-10-27 22:41:46 +1100

    move tour to models.py

---
+++ turtle_interface/src/app/models.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+from config import basedir
+
+tour_path = os.path.join(basedir, 'assets/tours/')
+
+class Tour:
+    def __init__(self, name):
+        self.name = name
+        self._file = os.path.join(tour_path, '{}.json'.format(self.name))
+
+    def load_tour(self):
+        with open(self._file, 'r') as f:
+            return json.load(f)
+        return None
+
+    def save_tour(self, data):
+        print 'Tour saved: {}'.format(self.name)
+        # add id to waypoints
+        for i, wp in enumerate(data['waypoints']):
+            wp.update({'id':i})
+        with open(self._file, 'wt') as f:
+            json.dump(data, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+def get_tour_list():
+    return [name for name, ext in map(os.path.splitext, os.listdir(tour_path)) if ext == '.json']
